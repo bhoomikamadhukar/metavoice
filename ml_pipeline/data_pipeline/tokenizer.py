3b4370bc73e6cfd09b186509dd7b7badf59cb5fb
Bhoomika <42411552+bhoomikamadhukar@users.noreply.github.com>
2025-05-05 22:13:55 -0400

    Add docstrings

--- ml_pipeline/data_pipeline/tokenizer.py
+++ ml_pipeline/data_pipeline/tokenizer.py
@@ -3,6 +3,15 @@ import time
 import numpy as np
 
 def tokenise(audio_np_array: np.ndarray) -> list:
+     """
+    Function to tokenise an audio file represented as a NumPy array.
+
+    Args:
+    - audio_np_array (np.ndarray): The audio file as a NumPy array.
+
+    Returns:
+    - torch.Tensor: A random 1D tensor with dtype int16 and variable length in range (20, 1000).
+    """
     if not torch.cuda.is_available():
         raise EnvironmentError("CUDA is not available.")
     del audio_np_array

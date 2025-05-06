9571f1060bb67d772861d968f61d5c25d4c7a594
Bhoomika <42411552+bhoomikamadhukar@users.noreply.github.com>
2025-05-05 22:12:36 -0400

    Add docstrings

--- ml_pipeline/data_pipeline/load_data.py
+++ ml_pipeline/data_pipeline/load_data.py
@@ -4,6 +4,9 @@ from io import BytesIO
 from .config import ACCESS_KEY, SECRET_KEY, ENDPOINT_URL, BUCKET_NAME, REGION_NAME
 
 def get_s3_client():
+    '''
+    Read data from s3 based on creds and create a client.
+    '''
     return boto3.client(
         's3',
         endpoint_url=ENDPOINT_URL,
@@ -14,9 +17,15 @@ def get_s3_client():
     )
 
 def list_audio_files(s3_client):
+    '''
+    Get all the audio files from the bucket and get the content of each audio file which ends with flac. 
+    '''
     response = s3_client.list_objects_v2(Bucket=BUCKET_NAME)
     return [obj["Key"] for obj in response.get("Contents", []) if obj["Key"].endswith(".flac")]
 
 def fetch_audio_stream(s3_client, key):
+    '''
+    The audio files are response objects and we need their bodies, so use the get object and io streaming to get the response body. 
+    '''
     response = s3_client.get_object(Bucket=BUCKET_NAME, Key=key)
     return BytesIO(response['Body'].read())
